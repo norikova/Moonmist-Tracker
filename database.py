a802a556cde194a91205216357a82e4f22049910
norikova <milkahyilka@gmail.com>
2026-07-31 18:24:57 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,66 +1,111 @@
+import os
 import sqlite3
 
+DATABASE_URL = os.environ.get("DATABASE_URL")
 DB = "moonmist.db"
-
 
 
 def get_connection():
 
+    # Render / PostgreSQL
+    if DATABASE_URL:
+
+        import psycopg2
+        from psycopg2.extras import RealDictCursor
+
+        conn = psycopg2.connect(DATABASE_URL)
+        conn.cursor_factory = RealDictCursor
+
+        return conn
+
+    # Локально / SQLite
     conn = sqlite3.connect(DB)
     conn.row_factory = sqlite3.Row
 
     return conn
 
 
-
 def init_db():
 
     conn = get_connection()
-
     cursor = conn.cursor()
 
+    if DATABASE_URL:
 
-    cursor.execute("""
-    CREATE TABLE IF NOT EXISTS kills (
+        # PostgreSQL
+        cursor.execute("""
+        CREATE TABLE IF NOT EXISTS kills (
 
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
+            id SERIAL PRIMARY KEY,
 
-        boss_id INTEGER,
+            boss_id INTEGER,
 
-        boss_name TEXT,
+            boss_name TEXT,
 
-        location TEXT,
+            location TEXT,
 
-        attempts INTEGER,
+            attempts INTEGER,
 
-        date TEXT
+            date TEXT
 
-    )
-    """)
+        )
+        """)
 
+        cursor.execute("""
+        CREATE TABLE IF NOT EXISTS attempts (
 
+            id SERIAL PRIMARY KEY,
 
-    cursor.execute("""
-    CREATE TABLE IF NOT EXISTS attempts (
+            boss_id INTEGER,
 
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
+            boss_name TEXT,
 
-        boss_id INTEGER,
+            location TEXT,
 
-        boss_name TEXT,
+            date TEXT
 
-        location TEXT,
+        )
+        """)
 
-        date TEXT
+    else:
 
-    )
-    """)
+        # SQLite
+        cursor.execute("""
+        CREATE TABLE IF NOT EXISTS kills (
 
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
 
+            boss_id INTEGER,
+
+            boss_name TEXT,
+
+            location TEXT,
+
+            attempts INTEGER,
+
+            date TEXT
+
+        )
+        """)
+
+        cursor.execute("""
+        CREATE TABLE IF NOT EXISTS attempts (
+
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+
+            boss_id INTEGER,
+
+            boss_name TEXT,
+
+            location TEXT,
+
+            date TEXT
+
+        )
+        """)
 
     conn.commit()
     conn.close()
-
 
 
 if __name__ == "__main__":
